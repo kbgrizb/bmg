322813cf4228f74782c2815d7ea6d3303a174961
kbgrizb <grisbykb@hendrix.edu>
2025-03-14 10:59:13 -0500

    Movement working, growth not working

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@
 use num::Integer;
 use pc_keyboard::{DecodedKey, KeyCode};
 use pluggable_interrupt_os::vga_buffer::{
-    is_drawable, plot, Color, ColorCode, BUFFER_HEIGHT, BUFFER_WIDTH,
+    is_drawable, plot, plot_num, Color, ColorCode, BUFFER_HEIGHT, BUFFER_WIDTH
 };
 
 use core::{
@@ -15,16 +15,32 @@ use core::{
 };
 
 #[derive(Copy, Clone, Eq, PartialEq)]
+pub struct NewSnakeChar {
+    x: usize,
+    y: usize,
+    character: char,
+}
+
+
+
+#[derive(Copy, Clone, Eq, PartialEq)]
 pub struct LetterMover {
     letters: [char; BUFFER_WIDTH],
     num_letters: usize,
     next_letter: usize,
     col: usize,
     row: usize,
-    dx: usize,
-    dy: usize,
+    dx: isize,
+    dy: isize,
     direction: Direction,
+    x_points: [usize; 100],
+    y_points: [usize; 100],
+    points_used: usize,
+    new_snake: NewSnakeChar,
+    score: isize
+, 
 }
+
 
 #[derive(Copy, Clone, Eq, PartialEq)]
 pub enum Direction{
@@ -33,6 +49,7 @@ pub enum Direction{
     Left,
     Right,
 }
+
 
 pub fn safe_add<const LIMIT: usize>(a: usize, b: usize) -> usize {
     (a + b).mod_floor(&LIMIT)
@@ -46,53 +63,135 @@ pub fn sub1<const LIMIT: usize>(value: usize) -> usize {
     safe_add::<LIMIT>(value, LIMIT - 1)
 }
 
+
 impl Default for LetterMover {
     fn default() -> Self {
+        
+
         Self {
             letters: ['o'; BUFFER_WIDTH],
             num_letters: 1,
             next_letter: 1,
-            col: BUFFER_WIDTH / 2,
+            col : BUFFER_WIDTH / 2,
             row: BUFFER_HEIGHT / 2,
             dx: 0,
             dy: 0,
             direction: Direction::Right,
+            x_points: [10; 100],
+            y_points: [10; 100],
+            points_used: 1,
+            new_snake: NewSnakeChar::new(),
+            score: 0
         }
     }
 }
 
-impl LetterMover {
-    fn letter_columns(&self) -> impl Iterator<Item = usize> + '_ {
-        (0..self.num_letters).map(|n| safe_add::<BUFFER_WIDTH>(n, self.col))
+impl NewSnakeChar {
+    fn new() -> Self {
+        Self {
+            x: (BUFFER_WIDTH / 2)+10,
+            y: (BUFFER_HEIGHT / 2)+10,
+            character: 'o', 
+        }
     }
+
+    fn draw(&self) {
+        plot(
+            self.character,
+            self.x,
+            self.y,
+            ColorCode::new(Color::Red, Color::Red),
+        );
+    }
+
+    fn clear_current(&self) {
+        plot(' ', self.x, self.y, ColorCode::new(Color::Black, Color::Black));
+    }
+}
+
+impl LetterMover {
+   // fn letter_columns(&self) -> impl Iterator<Item = usize> + '_ {
+     //   (0..self.num_letters).map(|n| safe_add::<BUFFER_WIDTH>(n, self.col))
+    //}
+
 
     pub fn tick(&mut self) {
         self.clear_current();
         self.update_location();
+    
+        if self.col == self.new_snake.x && self.row == self.new_snake.y {
+            self.handle_unicode(self.new_snake.character);
+            self.handle_add(self.new_snake.character,self.new_snake.x, self.new_snake.y);
+            self.new_snake.clear_current();
+            self.clear_food();
+            self.new_food();
+            self.score += 1;
+            self.points_used += 1;
+
+
+        }
+    
         self.draw_current();
+        self.new_snake.draw();
+        
     }
 
+    
+
     fn clear_current(&self) {
-        for x in self.letter_columns() {
-            plot(' ', x, self.row, ColorCode::new(Color::Black, Color::Black));
-        }
+        plot(' ', self.col, self.row, ColorCode::new(Color::Black, Color::Black));
+        
+    }
+    fn clear_food(&self) {
+        plot(' ', self.new_snake.x, self.new_snake.y, ColorCode::new(Color::Black, Color::Black));
+    }
+
+    fn new_food(&mut self) {
+        let seed = unsafe { core::arch::x86_64::_rdtsc() }; 
+        let mut rng = oorandom::Rand32::new(seed);
+        let new_x = rng.rand_range(0..(BUFFER_WIDTH as u32)) as usize;
+        let new_y = rng.rand_range(0..(BUFFER_HEIGHT as u32)) as usize;
+        self.clear_food();
+        self.new_snake = NewSnakeChar {
+            x: new_x,
+            y: new_y,
+            character: 'o',
+        };
     }
 
     fn update_location(&mut self) {
-        self.col = safe_add::<BUFFER_WIDTH>(self.col, self.dx);
-        self.row = safe_add::<BUFFER_HEIGHT>(self.row, self.dy);
+        
+    self.col = self.x_points[0];
+    self.row = self.y_points[0];
+    let new_x = self.x_points[0] as isize + self.dx;
+    let new_y = self.y_points[0] as isize + self.dy;
+    
+    if 0 <= new_x && (new_x as usize) < BUFFER_WIDTH {
+        self.x_points[0] = new_x as usize;
+    }
+    if 0 <= new_y && (new_y as usize) < BUFFER_HEIGHT{
+        self.y_points[0] = new_y as usize;
+    }
     }
 
     fn draw_current(&self) {
-        for (i, x) in self.letter_columns().enumerate() {
+        for i in 0..self.points_used{
             plot(
                 self.letters[i],
-                x,
-                self.row,
-                ColorCode::new(Color::Cyan, Color::Black),
+                self.x_points[i],
+                self.y_points[i],
+                ColorCode::new(Color::Green, Color::Green),
             );
         }
+        plot_num(
+            self.score,
+            5,
+            2,
+            ColorCode::new(Color::White, Color::Black)
+        );
+
     }
+
 
     pub fn key(&mut self, key: DecodedKey) {
         match key {
@@ -105,37 +204,48 @@ impl LetterMover {
         match key {
             KeyCode::ArrowLeft => {
                 if self.dx == 0{
-                    self.dx = sub1::<BUFFER_WIDTH>(self.dx);
+                    self.dx = -1;
                     self.dy = 0;
                     self.direction = Direction::Left;
+                    
                 }
         
             }
             KeyCode::ArrowRight => {
                 if self.dx == 0{
-                    self.dx = add1::<BUFFER_WIDTH>(self.dx);
+                    self.dx = 1;
                     self.dy = 0;
                     self.direction = Direction::Right;
+                    
                 }
                 
             }
             KeyCode::ArrowUp => {
                 if self.dy == 0{
-                    self.dy = sub1::<BUFFER_HEIGHT>(self.dy);
+                    self.dy = -1;
                     self.dx = 0;
                     self.direction = Direction::Up;
+                    
                 }
                 
             }
             KeyCode::ArrowDown => {
                 if self.dy == 0{
-                    self.dy = add1::<BUFFER_HEIGHT>(self.dy);
+                    self.dy = 1;
                     self.dx = 0;
                     self.direction = Direction::Down;
+                    
                 }
                 
             }
             _ => {}
+        }
+    }
+
+    fn handle_add(&mut self, key: char,x: usize, y: usize) {
+        if is_drawable(key) {
+            self.x_points[self.next_letter-1] = x;
+            self.y_points[self.next_letter-1] = y;
         }
     }
 
